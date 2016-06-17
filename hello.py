838ef231a880f058be3e767f05dc82c50256383b
peterzhou2016 <zhouping_02@163.com>
2016-06-17 17:11:09 +0800

    第一个学习文件

---
+++ hello.py
@@ -0,0 +1,37 @@
+# codeing:utf-8
+# print('hello world!')
+# print("hello world!")
+# print('我','是','周平')
+
+# name = input('请输入你的姓名：')
+
+# print('你的名字是：'+name)
+
+# a =int( input('请输入乘数：'))
+# b = int(input('请输入被乘数:'))
+# print('结果 =',a*b)
+
+# print('\\\n啊\t大海')
+# print(r'\\\n啊\t大海')
+# print('''
+# akj
+# adfa
+# adf
+# aytwrt
+# wrtw
+# ''')
+# -------
+# age = int(input('请输入你的年龄：'))
+# if age>18:
+#     print('你是成年人')
+# else:
+#     print('你是未成年人')	
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(n,f,s1,s2,s3,s4,sep='\n')
